3b379351eee310ad6c17f722803e86f70295ba21
Alistair Burrowes <alistair.burrowes@gmail.com>
2018-02-25 09:12:18 +1100

    app monad style working

--- api/src/Lib.hs
+++ api/src/Lib.hs
@@ -1,22 +1,23 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
-{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeOperators              #-}
 module Lib
     ( startApp
     , app
     ) where
 
-import           Control.Monad.Logger     (LoggingT, runStdoutLoggingT, MonadLogger)
-import           Control.Monad.Reader     (ReaderT, runReaderT, MonadReader)
+import           Control.Monad.IO.Class   (MonadIO, liftIO)
+import           Control.Monad.Logger     (LoggingT, MonadLogger,
+                                           runStdoutLoggingT)
+import           Control.Monad.Reader     (MonadReader, ReaderT, runReaderT)
 import           Data.Aeson.TH            (defaultOptions, deriveJSON)
 import           Data.Maybe               (listToMaybe)
 import           Network.Wai              (Application)
 import           Network.Wai.Handler.Warp (run)
-import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Servant
-import Servant.Server (hoistServer)
+import           Servant.Server           (hoistServer)
 
 data Todo = Todo
   { todoId  :: Int
@@ -35,42 +36,36 @@ newtype App a = App { unApp :: LoggingT (ReaderT AppConfig IO) a }
 appToHandler :: AppConfig -> App a -> Handler a
 appToHandler config = liftIO . flip runReaderT config . runStdoutLoggingT . unApp
 
-type TodoAPI = "todos" :> Get '[JSON] [Todo]
+theConfig :: AppConfig
+theConfig = AppConfig "host" "user" "pass"
 
-v2Server :: Server TodoAPI
-v2Server = hoistServer api appToHandler v2ServerBack
-
--- omg = hoistServer api
+server :: Server TodoAPI
+server = hoistServer api (appToHandler theConfig) backend
 
 startApp :: IO ()
 startApp = run 8080 app
 
 app :: Application
-app = serve api v2Server
+app = serve api server
 
 api :: Proxy TodoAPI
 api = Proxy
 
--- server :: Server TodoAPI
--- server = getTodos
+backend :: ServerT TodoAPI App
+backend = getTodos :<|> getTodo :<|> addTodo
 
-v2ServerBack :: ServerT TodoAPI App
-v2ServerBack = v2getTodos
+getTodos :: App [Todo]
+getTodos = pure todos
 
--- getTodos :: Handler [Todo]
--- getTodos = pure todos
+getTodo :: Int -> App (Maybe Todo)
+getTodo todoId' = pure $ listToMaybe $ filter ((==) todoId' . todoId) todos
 
-v2getTodos :: App [Todo]
-v2getTodos = pure todos
+addTodo :: Todo -> App Int
+addTodo _ = pure 1
 
--- getTodo :: Int -> Handler (Maybe Todo)
--- getTodo todoId' = pure $ listToMaybe $ filter ((==) todoId' . todoId) todos
-
--- addTodo :: Todo -> Handler Int
--- addTodo todo = pure 1
-
--- :<|> "todos" :> Capture "id" Int :> Get '[JSON] (Maybe Todo)
--- :<|> "todos" :> ReqBody '[JSON] Todo :> Post '[JSON] Int
+type TodoAPI = "todos" :> Get '[JSON] [Todo]
+          :<|> "todos" :> Capture "id" Int :> Get '[JSON] (Maybe Todo)
+          :<|> "todos" :> ReqBody '[JSON] Todo :> Post '[JSON] Int
 
 todos :: [Todo]
 todos =

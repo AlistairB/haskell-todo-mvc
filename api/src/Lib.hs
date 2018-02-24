c68986638efa8621b3a294386d54cb6c624eb7e5
Alistair Burrowes <alistair.burrowes@gmail.com>
2018-02-24 15:24:04 +1100

    Hello world servant working

--- api/src/Lib.hs
+++ api/src/Lib.hs
@@ -1,6 +1,63 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
 module Lib
-    ( someFunc
+    ( startApp
+    , app
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Data.Int                 (Int64)
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant                  ((:<|>), (:>), Capture, Get, JSON,
+                                           Post, Proxy (Proxy), ReqBody, Server,
+                                           serve)
+
+-- data Todo = Todo
+--   { id :: Int
+--   , message :: String
+--   } deriving Show
+
+data User = User
+  { userId        :: Int
+  , userFirstName :: String
+  , userLastName  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+type API = "users" :> Get '[JSON] [User]
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return users
+
+users :: [User]
+users = [ User 1 "Isaac" "Newton"
+        , User 2 "Albert" "Einstein"
+        ]
+
+-- type Api = "todos" :> Capture "id" Int :> Get '[JSON] Todo
+--       :<|> "todos" :> Capture "name" String :> Get '[JSON] Todo
+--       :<|> "todos" :> ReqBody '[JSON] Todo :> Post '[JSON] Int64
+
+
+-- todos :: [Todo]
+-- todos =
+--   [ Todo 1 "Write haskell api"
+--   , Todo 2 "Write purescript frontend"
+--   ]
+
+-- getTodo :: Int -> Server Todo
+-- getTodo id = pure $ Todo 1 "omg"
+

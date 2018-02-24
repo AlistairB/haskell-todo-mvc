0d2dea69da0b07fa943b39a2586ad4218a551437
Alistair Burrowes <alistair.burrowes@gmail.com>
2018-02-24 21:47:46 +1100

    Attempting to get servant custom monad style to work

--- api/src/Lib.hs
+++ api/src/Lib.hs
@@ -1,48 +1,76 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Lib
     ( startApp
     , app
     ) where
 
+import           Control.Monad.Logger     (LoggingT, runStdoutLoggingT, MonadLogger)
+import           Control.Monad.Reader     (ReaderT, runReaderT, MonadReader)
 import           Data.Aeson.TH            (defaultOptions, deriveJSON)
 import           Data.Maybe               (listToMaybe)
 import           Network.Wai              (Application)
 import           Network.Wai.Handler.Warp (run)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Servant
+import Servant.Server (hoistServer)
 
 data Todo = Todo
   { todoId  :: Int
   , message :: String
   } deriving Show
 
-$(deriveJSON defaultOptions ''Todo)
+data AppConfig = AppConfig
+  { dbHost :: String
+  , dbUser :: String
+  , dbPass :: String
+  }
+
+newtype App a = App { unApp :: LoggingT (ReaderT AppConfig IO) a }
+  deriving (Functor, Applicative, Monad, MonadIO)
+
+appToHandler :: AppConfig -> App a -> Handler a
+appToHandler config = liftIO . flip runReaderT config . runStdoutLoggingT . unApp
+
+type TodoAPI = "todos" :> Get '[JSON] [Todo]
+
+v2Server :: Server TodoAPI
+v2Server = hoistServer api appToHandler v2ServerBack
+
+-- omg = hoistServer api
 
 startApp :: IO ()
 startApp = run 8080 app
 
 app :: Application
-app = serve api server
+app = serve api v2Server
 
-api :: Proxy API
+api :: Proxy TodoAPI
 api = Proxy
 
-server :: Server API
-server = getTodos :<|> getTodo :<|> addTodo
+-- server :: Server TodoAPI
+-- server = getTodos
 
-getTodos :: Handler [Todo]
-getTodos = pure todos
+v2ServerBack :: ServerT TodoAPI App
+v2ServerBack = v2getTodos
 
-getTodo :: Int -> Handler (Maybe Todo)
-getTodo todoId' = pure $ listToMaybe $ filter ((==) todoId' . todoId) todos
+-- getTodos :: Handler [Todo]
+-- getTodos = pure todos
 
-addTodo :: Todo -> Handler Int
-addTodo todo = pure 1
+v2getTodos :: App [Todo]
+v2getTodos = pure todos
 
-type API = "todos" :> Get '[JSON] [Todo]
-      :<|> "todos" :> Capture "id" Int :> Get '[JSON] (Maybe Todo)
-      :<|> "todos" :> ReqBody '[JSON] Todo :> Post '[JSON] Int
+-- getTodo :: Int -> Handler (Maybe Todo)
+-- getTodo todoId' = pure $ listToMaybe $ filter ((==) todoId' . todoId) todos
+
+-- addTodo :: Todo -> Handler Int
+-- addTodo todo = pure 1
+
+-- :<|> "todos" :> Capture "id" Int :> Get '[JSON] (Maybe Todo)
+-- :<|> "todos" :> ReqBody '[JSON] Todo :> Post '[JSON] Int
 
 todos :: [Todo]
 todos =
@@ -50,3 +78,4 @@ todos =
   , Todo 2 "Write purescript frontend"
   ]
 
+$(deriveJSON defaultOptions ''Todo)
